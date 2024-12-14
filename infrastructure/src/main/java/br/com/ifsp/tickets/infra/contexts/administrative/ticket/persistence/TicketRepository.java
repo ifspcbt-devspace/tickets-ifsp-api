c2cd83b1c045ad8366f5b9d1b97baba7097fefde
Leonardo <silva.leonardo2@aluno.ifsp.edu.br>
2024-12-14 09:49:13 -0300

    fix: dependencies

--- infrastructure/src/main/java/br/com/ifsp/tickets/infra/contexts/administrative/ticket/persistence/TicketRepository.java
+++ infrastructure/src/main/java/br/com/ifsp/tickets/infra/contexts/administrative/ticket/persistence/TicketRepository.java
@@ -4,11 +4,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface TicketRepository extends JpaRepository<TicketJpaEntity, UUID> {
-    Optional<TicketJpaEntity> findByCodeAndExpiredInBefore(String code, java.util.Date date);
-    Page<TicketJpaEntity> findAllByUserId(UUID userId, Pageable pageable);
-    Page<TicketJpaEntity> findAllByUserIdAndEventId(UUID userId, UUID eventId, Pageable pageable);
+
+    Optional<TicketJpaEntity> findByCodeAndExpiredInBefore(String code, LocalDate date);
+
+    Page<TicketJpaEntity> findAllByEnrollmentUserID(UUID userId, Pageable pageable);
+
+    Page<TicketJpaEntity> findAllByEnrollmentUserIDAndEventId(UUID userId, UUID eventId, Pageable pageable);
+
 }
